513bb478bd5f98c409d5c188ade21ea497cd2f0e
Emee <emeli.yuliana18@gmail.com>
2023-10-16 21:01:33 -0500

    Se modifican configuraciones

--- mysite/settings.py
+++ mysite/settings.py
@@ -35,8 +35,6 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'tresko.apps.MyAccountManager',
-    'tresko.apps.Workspace',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
